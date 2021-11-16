a39e12fd40f950f01ca2c18f9291830fc3943538
pawel-golab <87443171+pawel-golab@users.noreply.github.com>
2021-11-16 15:13:37 +0100

    Create Arrays.class.php

---
+++ array/Arrays.class.php
@@ -0,0 +1,78 @@
+<?php
+
+Class Arrays
+{
+    public $is;
+    public $Array = [];
+
+    public function __construct(...$values)
+    {
+        $this->Array = $values;
+        $this->is = new Is($this->Array);
+    }
+    public function __toString()
+    {
+        $output = '';
+        foreach( $this->Array as $i => $value ){
+            $type = match(gettype($value)){
+                'boolean'      => 'b',
+                'integer'   => 'i',
+                'float'     => 'f',
+                'string'    => 's',
+                'array'     => 'A',
+                'object'    => 'O',
+                default     => 'x'
+
+            };
+            if( $type == 'A' )
+                $output .= "$i => [?]\t";
+            else
+                $output .= "$i => $value ($type)\t"; 
+        }
+        return $output;
+    }
+    public function __call( $name, $arguments )
+    {
+        try{
+            return call_user_func_array( 'array_' . $name , [$this->Array, ...$arguments] );
+        }
+        catch(TypeError $e){
+            return call_user_func_array( $name , [$this->Array, ...$arguments] );
+        }
+        finally{
+            return false;
+        }
+    }
+    public function __invoke( $offset = null )
+    {
+        return $this->Array[$offset] ?? 0;
+    }
+    public function arrayType() :string
+    {
+        $indexed = 0;
+        $associative = 0;
+
+        foreach( $this->Array as $key => $value ){
+            if(gettype($key) == 'integer')
+                $indexed++;
+            else
+                $associative++;
+
+            if($indexed > 0 && $associative > 0)
+                return 'mixed';
+        }
+        if($indexed > 0) return 'indexed'; return 'assiciative';
+    }
+    public function type() :string
+    {
+        $array = $this->Array;
+        $searched = gettype(array_pop($array));
+
+        foreach( $array as $value ){
+            if( $searched != gettype($value) )
+                return "mixed";
+        }
+
+        return $searched;
+    }
+}
