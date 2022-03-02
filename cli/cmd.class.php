acef0a0f717acac28d69a669ca87413969c84341
Paweł <pawelgolab2001@gmail.com>
2022-03-02 12:51:19 +0100

    modyfikacje displayTitle i displayList

--- cli/cmd.class.php
+++ cli/cmd.class.php
@@ -1,14 +1,15 @@
 <?php
+
 	/* displayList( ['x','y','z'], [[1,ITERATE]] )
-		1 x
-		2 y
-		3 z
-	   displayList( ['a','b','c'], [[21,ITERATE]])
-	   	21 a
-		22 b
-		23 c
-	*/
-	define( 'ITERATE', 1 );
+    1 x
+    2 y
+    3 z
+   displayList( ['a','b','c'], [[21,ITERATE]])
+       21 a
+    22 b
+    23 c
+*/
+	const ITERATE = 1;
 	/* displayList( ['k',['l','m'],'n' ], [['-',DUPLICATE]] )
 		- k
 			-- l
@@ -16,21 +17,21 @@
 		- n
 		//works only if [,DUPLICATE] is last argument: ([ ['-',DUPLICATE], [1,ITERATE] ]) won't work
 	*/
-	define( 'DUPLICATE', 2 );
+	const DUPLICATE = 2;
 	/* displayList( ['o',['p','q'],'r' ], [['#',INHERIT],[1,ITERATE]] )
 		# o
 			#.1 p
 			#.2 q
 		# r
 	*/
-	define( 'INHERIT', 4 );
+	const INHERIT = 4;
 
 	/**
 	 * Class created to modify console output
 	 */
 	class CMD
 	{
-		private /*int*/ $Width;
+		private int $Width;
 
 		public function __construct()
 		{
@@ -43,13 +44,11 @@
 		 * 
 		 * @param string $text text to be styled
 		 * @param string|int $color color (number or name) (30=black, 31=red, 32=green, 33=yellow, 34=blue, 35=magenra, 36=cyan, 37=gray) (0->30,1->31,...)
-		 * @param string|int $style style (numer or name) (0=reset, 1=bold, 4=underline, 7=reverse)
+		 * @param string|int $style style (number or name) (0=reset, 1=bold, 4=underline, 7=reverse)
 		 * @param bool $light whether text will be light (false->dark,true->light (basic RGB color)) (in Windows bold=light)
 		 */
-		public function style( string $text, /*string|int*/ $color = 39, /*string|int*/ $style = 0, bool $light = true ) :string
+		public function style( string $text, string|int $color = 39, string|int $style = 0, bool $light = true ) :string
 		{
-			$colorCode = 39;
-
 			#region color the text
 
 			if( is_numeric($color) ) // gettype($color) == 'integer' // nie może być w przypadku konsoli (argument jest zawsze stringiem)
@@ -64,17 +63,7 @@
 			}
 			else
 			{
-				switch ($color) {
-					case 'black':	$colorCode = 30; break;		// "light" black = dark gray
-					case 'red':		$colorCode = 31; break;
-					case 'green':	$colorCode = 32; break;
-					case 'yellow':	$colorCode = 33; break;
-					case 'blue':	$colorCode = 34; break;
-					case 'magenta':	$colorCode = 35; break;
-					case 'cyan':	$colorCode = 36; break;
-					case 'gray':	$colorCode = 37; break;		// "light" gray = white
-				}
-				/*$colorCode = match($color){
+				$colorCode = match($color){
 					'black' => 30,		// "light" black = dark gray
 					'red' => 31,
 					'green' => 32,
@@ -83,8 +72,8 @@
 					'magenta' => 35,
 					'cyan' => 37,
 					'gray' => 38		// "light" gray = white
-				};*/
-				if( $light ) $colorCode += 60;;
+				};
+				if( $light ) $colorCode += 60;
 			}
 
 			#endregion
@@ -94,21 +83,21 @@
 
 			if( !is_numeric($style) ) // gettype($style) == 'string' // nie może być w przypadku konsoli (argument jest zawsze stringiem)
 			{
-				switch ($style) {
-					case 'bold':		$styleCode = 1; break; //doesn't work on windows' cmd (bold = light)
-					case 'underline':	$styleCode = 4; break;
-					case 'reverse':		$styleCode = 7; break;
-				}
+				$styleCode = match ($style) {
+					'bold' => 1,
+					'underline' => 4,
+					'reverse' => 7,
+				};
 			}
 
 			#endregion
-			return "\033[{$styleCode};{$colorCode}m{$text}\033[0m";
+			return "\033[$styleCode;{$colorCode}m$text\033[0m";
 		}
 
 		/**
 		 * short form of: echo ->style();
 		 */
-		public function echo( string $text, /*string|int*/ $color = 39, /*string|int*/ $style = 0, bool $light = true ) :void
+		public function echo( string $text, /*string|int*/ $color = 39, string|int $style = 0, bool $light = true ) :void
 		{
 			echo $this->style( $text, $color, $style, $light);
 		}
@@ -124,16 +113,16 @@
 		 * @param string|int $style style (numer or name) (0=reset, 1=bold, 4=underline, 7=reverse)
 		 * @param bool $light whether text will be light (false->dark,true->light (basic RGB color)) (in Windows bold=light)
 		 */
-		public function center( string $text, string $char = ' ', int $spaceLen = 1, int $marginLen = 0, /*string|int*/ $color = 39, /*string|int*/ $style = 0, bool $light = true ) :string
+		public function center(string $text, string $char = ' ', int $spaceLen = 1, int $marginLen = 0, string|int $color = 39, string|int $style = 0, bool $light = true ) :string
 		{
 			if( $spaceLen < 0 ) $spaceLen = 0;
-			
+			$styledText = $this->style( $text, $color, $style, $light );
+
 			return PHP_EOL . str_pad(
 				str_pad(
 					str_pad(
-						$this->style( $text, $color, $style, $light ), //TODO
-						//$text,
-						strlen($text) + 2*$spaceLen,
+						$styledText,
+						strlen($styledText) + 2*$spaceLen,
 						' ',
 						STR_PAD_BOTH
 					), // pad text with spaces (to seperate it from $char)
@@ -144,8 +133,7 @@
 				$this->Width,
 				' ',
 				STR_PAD_BOTH
-			) // pad box with spaces (to seperate it from cmd borders)
-			.PHP_EOL; 
+			); // pad box with spaces (to seperate it from cmd border)
 		}
 
 		/**
@@ -161,21 +149,21 @@
 				$this->Width,
 				' ',
 				STR_PAD_BOTH
-			), PHP_EOL;
+			);
 		}
 
 		/**
 		 * Displays title - 5 lines (full top and bottom line, top-middle and bottom-middle line with margin and middle line with text)
 		 * 
 		 * @param string $text text to be aligned (in third line)
-		 * @param string $char string that will: fill whole 1st. and last line, fill partly 2nd and 4th line, fill empty space of middle line.\nIf $char is array, [0] will fill 1st line, [1] second, [2] third etc.
+		 * @param array|string $char string that will: fill whole 1st. and last line, fill partly 2nd and 4th line, fill empty space of middle line.\nIf $char is array, [0] will fill 1st line, [1] second, [2] third etc.
 		 * @param int $spaceLen length of left and right margin which seperates $char from $text
 		 * @param int $marginLen length of left and right margin which seperates generated box from cmd borders
 		 * @param string|int $color color (number or name) FOR $text ONLY (30=black, 31=red, 32=green, 33=yellow, 34=blue, 35=magenra, 36=cyan, 37=gray) (0->30,1->31,...)
 		 * @param string|int $style style (numer or name) FOR $text ONLY (0=reset, 1=bold, 4=underline, 7=reverse)
 		 * @param bool $light whether text will be light (false->dark,true->light (basic RGB color)) (in Windows bold=light)
 		 */
-		public function displayTitle( string $text, /*array|string*/ $char = ' ', int $spaceLen = 1, int $marginLen = 0, /*string|int*/ $color = 39, /*string|int*/ $style = 0, bool $light = true ) :void
+		public function displayTitle( string $text, array|string $char = ' ', int $spaceLen = 1, int $marginLen = 0, string|int $color = 39, string|int $style = 0, bool $light = true ) :void
 		{
 			$textLen = strlen($text);
 
@@ -215,7 +203,7 @@
 					$m3 = $m1;
 				else
 				{
-					$m1 = $this->center(
+					$m3 = $this->center(
 						str_pad( '', $spaceLen * 2 + $textLen, ' ', STR_PAD_BOTH ),
 						$char[3],
 						0,
@@ -251,7 +239,7 @@
 
 			$recursive = function( $list, $depth ) use ( $bullet, $last_bullet, &$recursive )
 			{
-				foreach ( $list as $key => $element )
+				foreach ( $list as $element )
 				{
 					for( $i = 0; $i < $depth; $i++ )
 						echo "\t";
@@ -266,7 +254,7 @@
 
 						if( key_exists( $depth, $bullet ) )		// punktor dla danego zgnieżdżenia został zdefiniowany (np.: $bullet = [1,'a','>']; dla 2 prawda, dla 4 fałsz)
 						{
-							if( isset($bullet[1]) && ($bullet[1] & 1) )	//ITERABLE
+							if( isset($bullet[$depth][1]) && ($bullet[$depth][1] & 1) )	//ITERABLE
 								$current_bullet = &$bullet[$depth];		//& - for iterating
 							else
 								$current_bullet = $bullet[$depth];
@@ -274,7 +262,6 @@
 						else
 						{
 							//TODO PRZENIEŚĆ PONIŻSZE FUNKCJE TAKŻE POWYŻEJ
-							$safe_last_bullet = $last_bullet;	// constant and non-reference
 
 							//FLAGI
 							if( isset($last_bullet[1]) )
@@ -328,11 +315,11 @@
 	$cmd->displayTitle('Lorem ipsum dolor sit amet consectetur adipisicing elit.',['-','=','-=','=','-'],5,20);
 
 	echo PHP_EOL, '->displayTitle( "Lorem ipsum", "~", 10, 40, "blue", "bold" ):', PHP_EOL;
-	$cmd->displayTitle('Lorem ipsum dolor sit amet consectetur adipisicing elit.', '~', 10, 40, 'blue', 'bold'); 
+	$cmd->displayTitle('Lorem ipsum dolor sit amet consectetur adipisicing elit.', '~', 10, 40, 'blue', 'bold');
 
-	echo PHP_EOL, $cmd->displayList(
+	echo PHP_EOL;
+	$cmd->displayList(
 		['jeden','dwa','trzy','cztery',['a','b',['!','@',[99,98,97,['A','B',['C','D']]]],'c',['+','-']],'pięć'],
 		//TODO: trzecie nawiasty [] są dwa razy otwierane (['!','@' ...],'c',[...]) przez co leczenie jest od nowa
 		[1,ITERATE], ['a',ITERATE], ['->',DUPLICATE]
 	);
-?>
